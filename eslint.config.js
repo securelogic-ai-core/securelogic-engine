b90299ab4ce255580526304f535bc7303aef670f
SecureLogic-AI <info@securelogicai.com>
2025-12-26 20:34:28 +0000

    CHORE: migrate ESLint v9 config and finalize CI scripts

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        sourceType: "module",
+        ecmaVersion: "latest"
+      }
+    },
+    plugins: {
+      "@typescript-eslint": tseslint
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/consistent-type-imports": "error"
+    }
+  }
+];
